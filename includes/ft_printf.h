ac0550fea4242bc6bcb090dd5e9360906b21a037
Wei Jun Lim <weijunlimm@gmail.com>
2022-04-26 01:23:30 +0800

    ok

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -7,36 +7,21 @@
 # include "..libft/libft.h"
 
 //struct that stores types of format
-
-// typedef struct s_print
-// {
-// 	va_list	args;
-// 	int		tl;//total length
-// 	int		wdt;//width
-// 	int		prc;//precision
-// 	int		zero;// is number zero
-// 	int		pnt;// .
-// 	int		dash;// -
-// 	int		hash;
-// 	int		sign;//positive or negative number
-// 	int		spc;//space flags
-// 	int		cap_x;// X
-// }	t_print;
-
-typedef struct s_print
+typedef struct s_format
 {
 	va_list  args;//	arg to print out
-	int		wdt;//		width
-	int		prc;//		precision
-	int		zero;//		zero padding
-	int		dot;//		.
-	int		dash;//		-
-	int		tl;//		total_length (return value)
-	int		sign;//		positive or negative number
-	int		is_zero;//	is number zero
-	int		perc;//		%
-	int		sp;//		space flag ' '
-}		t_print;
+	int			plus;
+	int			prc;
+	int			dot;
+	int			minus;
+	int			count;
+	int			width;
+	int			space;
+	int			zero;
+	// int		cap_x;
+	int			hash;
+	int 		zeroes;
+}			t_format;
 
 //main function
 int ft_printf(const char *str, ...);
--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -1,43 +1,111 @@
 #include "ft_printf.h"
 
 //this function will reset the struct values to 0
-static t_format	*ft_reset_format(t_format *flags)
+static void	reset_format(t_format *flags)
 {
-	flags->wdt = 0;
+	flags->width = 0;
 	flags->prc = 0;
 	flags->zero = 0;
 	flags->dot = 0;
-	flags->dash = 0;
-	flags->tl = 0;
-	flags->sign = 0;
-	flags->is_zero = 0;
-	flags->perc = 0;
-	flags->sp = 0;
+	flags->hash = 0;
+	flags->plus = 0;
+	flags->minus = 0;
+	flags->space = 0;
+	// flags->cap_x = 0;
+	flags->count = 0;
 	return (flags);
+}
+
+//this function check for the flags
+static void	check_flags(char *str, t_format *input)
+{
+	if (*str == ' ')
+		flags->space = 1;
+	if (*str == '+')
+		flags->plus = 1;
+	if (*str == '-')
+		flags->minus = 1;
+	if (*str == '#')
+		flags->hash = 1;
+	if (*str == '0')
+		flags->zero = 1;
+}
+
+//calculate the width precision
+static char	*check_width_precision(char *str, t_format *flags)
+{
+	if (*str >= '1' && *str <= '9')
+	{
+		while (ft_isdigit(*str))
+			flags->width = flags->width * 10 + *str++ - 48;
+		str--;
+	}
+	if (*str == '.')
+	{
+		flags->dot = 1;
+		str++;
+		while (ft_isdigit(*str))
+			flags->precision = flags->precision * 10 + *str++ - 48;
+		str--;
+	}
+	return (str);
+}
+
+//check for specifier
+static int	*check_specifier(char *str, t_format *flags)
+{
+	while (*str && (!ft_strchr("csdiuxXp%", *str)))
+	{
+		check_flags(str, flags);
+		str = check_width_precision(str, flags);
+		str++;
+	}
+	if (*str == 'd' || *str == 'i')//	decimal
+		ft_printf_int(flags);
+	if (*str == 'c')//					character
+		ft_printf_char(flags);
+	if (*str == 's')//					string
+		ft_printf_str(flags);
+	if (*str == 'u')//					unsigned int
+		ft_printf_uint(flags);
+	if (*str == 'x' || *str == 'X')//	hexadecimal
+		ft_printf_hex(flags, *str);
+	if (*str == 'p')//					pointer
+		ft_printf_ptr(flags);
+	if (*str == '%')//					prints '%' character
+		flags->size += ft_putchar('%');
+	//str++
+	return (++str);
 }
 
 int ft_printf(const char *input, ...)
 {
 	int output;
-	int i;
-	t_print *flags;
+	char	*str;
+	t_format *flags;
 
 	output = 0;
-	flags = (t_print *)malloc(sizeof(t_print));
+	flags = (t_format *)malloc(sizeof(t_format));
 	if (!flags)
 		return (-1);
-	ft_reset_format(flags);
 	va_start(flags->args, input);
-	i = -1;
-	while (input[++i])//	while the string exists
+	flags->size = 0;
+	while (*str)//	while the string exists
 	{
-		if (input[i] == '%')
-			i = ft_handle_format(flags, (char *)input, i + 1);//i + 1 because we start evaluate after the % sign
+		if (*str == '%')
+		{
+			reset_format(flags);
+			str = check_specifier(++str, flags);
+			//str = check_specifier(flags, (char *)input, i + 1);//i + 1 because we start evaluate after the % sign
+		}
 		else
-			output += write(1, &input[i], 1);
+		{
+			output += write(1, str++, 1);
+		}
+		// res += flags->count;
 	}
 	va_end(flags->args);
-	output += flags->tl;
+	output += flags->size;
 	free(flags);
 	return (output);
 }
